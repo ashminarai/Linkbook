258073136b5991432540f74ca2567b06e08a5293
ashmina <raiashmina404@gmail.com>
2023-06-08 01:10:10 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Index from './Components/Basics/Index';"./Components/Basics/Index";
 
 function App() {
-  <>
-  </>
+  return(
+    <>
+    <Index/>
+    </>
+  )
+ 
 }
 
 export default App;
--- src/Components/Basics/Index.js
+++ src/Components/Basics/Index.js
@@ -4,7 +4,8 @@ const Index = () => {
  <>
  <div className='navigation-container'>
     <div className='navigation-container-content'>
-
+      <h2>facebook</h2>
+      <h5>Connect with friends and the world around you on Facebook.</h5>
     </div>
 
  </div>
