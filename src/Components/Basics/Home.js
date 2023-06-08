7d4667d7666034f881d501bcbff7ac77cede78af
ashmina <raiashmina404@gmail.com>
2023-06-08 10:24:58 +0545

    initialize

--- src/Components/Basics/Home.js
+++ src/Components/Basics/Home.js
@@ -4,11 +4,37 @@ import "./Style.css";
 const Home = () => {
   return (
     <>
-      <div className="navigation-container">
-        <div className="navigation-container-content">
-          <h2 className="nfb">facebook</h2>
-          <h5 className="connect">Connect with friends and the world around you on Facebook.</h5>
+      {/* <div className="home-container">
+        <div className="navigation-container">
+          <div className="navigation-container-content">
+            <h2 className="nfb">hi</h2>
+            <h2 className="nfb">hi</h2>
+
+          </div>
+
+          <div className="form-container">
+            <div className="form-container-content">
+              <input>
+              </input>
+            </div>
+          </div>
         </div>
+      </div> */}
+
+      <div>
+        <h2>Welcome to Facebook</h2>
+        <p>Log in to connect with friends and the world around you.</p>
+        <p>
+          <strong>Email or Phone</strong>
+        </p>
+      </div>
+      <div className="login-container">
+        <div className="logo">Facebook</div>
+        <form className="login-form">
+          <input type="text" placeholder="Email or Phone" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Log In</button>
+        </form>
       </div>
     </>
   );
