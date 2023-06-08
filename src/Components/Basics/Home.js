412d0e5bb8f17ce90f596146d5010ace97eb7981
ashmina <raiashmina404@gmail.com>
2023-06-08 10:05:18 +0545

    commited

--- src/Components/Basics/Home.js
+++ src/Components/Basics/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Style.css";
 
 const Home = () => {
   return (
     <>
       <div className="navigation-container">
         <div className="navigation-container-content">
-          <h2>facebook</h2>
-          <h5>Connect with friends and the world around you on Facebook.</h5>
+          <h2 className="nfb">facebook</h2>
+          <h5 className="connect">Connect with friends and the world around you on Facebook.</h5>
         </div>
       </div>
     </>
