bd071fdcdaa9cd6fb3c08e504e3d13cae27c58db
ashmina <raiashmina404@gmail.com>
2023-06-13 23:34:21 +0545

    sad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-import Home from './Components/Basics/Home';
-import Login from './Components/Basics/Login';
-import Photo from './Components/Basics/Photo';
-
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Basics/Home";
+import Login from "./Components/Basics/Login";
+import Photo from "./Components/Basics/Photo";
+import Register from "./Components/Basics/Register";
 
 function App() {
-  return(
+  return (
     <>
-     <Router>
-     <Home/>
-     <Login/>
-     <Photo/>
-     {/* {/* <Route path="/" exact component={Home} /> */} 
-    </Router>
-  
-    
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Home />}></Route>
+        </Routes>
+        <Home />
+        <Login />
+        <Photo />
+
+        {/* {/* <Route path="/" exact component={Home} /> */}
+      </Router>
     </>
-  )
- 
+  );
 }
 
 export default App;
---
+++ src/Components/Basics/Register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Style.css";
+
+const Register = () => {
+  return (
+    <>
+      <div>
+        hello
+      </div>
+    </>
+  );
+};
+
+export default Register;
