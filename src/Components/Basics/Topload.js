da99e150726c8887d0903294fd614aec6522b6f7
ashmina <raiashmina404@gmail.com>
2023-06-14 11:19:54 +0545

    topload commited

--- src/Components/Basics/Topload.js
+++ src/Components/Basics/Topload.js
@@ -12,24 +12,29 @@ const Topload = () => {
           </div>
           <div className="topbar-container1">
             <i class="fa-solid fa-magnifying-glass"></i>
-            <input type="text" placeholder="Search Linkbook"className="search"></input>
+            <input
+              type="text"
+              placeholder="Search Linkbook"
+              className="search"
+            ></input>
+          </div>
+
+          <div className="topbar-container2">
+            <i class="fa-sharp fa-solid fa-house"></i>
           </div>
           <div className="topbar-container2">
-          <i class="fa-sharp fa-solid fa-house"></i>
+            <i class="fa-solid fa-user-group"></i>
+          </div>
+
+          <div className="topbar-container2">
+          <i class="fa-solid fa-store"></i>
           </div>
           <div className="topbar-container2">
-          <i class="fa-sharp fa-solid fa-house"></i>
+          <i class="fa-solid fa-people-group"></i>
           </div>
           <div className="topbar-container2">
-          <i class="fa-sharp fa-solid fa-house"></i>
+          <i class="fa-solid fa-gamepad"></i>
           </div>
-          <div className="topbar-container2">
-          <i class="fa-sharp fa-solid fa-house"></i>
-          </div>
-          <div className="topbar-container2">
-          <i class="fa-sharp fa-solid fa-house"></i>
-          </div>
-          
         </div>
       </div>
     </>
