eb22e3d205f3ab3b4c17990f1628afdecadced83
ashmina <raiashmina404@gmail.com>
2023-06-14 15:45:26 +0545

    updated

--- src/Components/Basics/Topload.js
+++ src/Components/Basics/Topload.js
@@ -10,9 +10,8 @@ const Topload = () => {
         <div className="topbarcontent">
           <div className="topbar-container">
             <Link to="/">
-            <img src="../Images/logo.png" className="logo"></img>
+              <img src="../Images/logo.png" className="logo"></img>
             </Link>
-            
           </div>
           <div className="topbar-container1">
             <i class="fa-solid fa-magnifying-glass"></i>
@@ -22,24 +21,33 @@ const Topload = () => {
               className="search"
             ></input>
           </div>
-          <div className="topbar-container2">
-            <i class="fa-sharp fa-solid fa-house"></i>
+          <div class="container">
+            {/* <div class="hover-element"> */}
+            <i class="fa-sharp fa-solid fa-house hover-element"></i>
+              <span class="text">Home</span>
+            </div>
           </div>
-          <div className="topbar-container2">
+
+          {/* <div className="topbar-container2">
+            <i class="fa-sharp fa-solid fa-house"></i>
+            <br />
+            <br />
+            <span className="fa-sharps">Home</span>
+          </div> */}
+          {/* <div className="topbar-container3">
             <i class="fa-solid fa-user-group"></i>
           </div>
 
-          <div className="topbar-container3">
+          <div className="topbar-container4">
             <i class="fa-solid fa-store"></i>
           </div>
-          <div className="topbar-container4">
+          <div className="topbar-container5">
             <i class="fa-solid fa-people-group"></i>
           </div>
-          <div className="topbar-container5">
+          <div className="topbar-container6">
             <i class="fa-solid fa-gamepad"></i>
-          </div>
+          </div> */}
         </div>
-      </div>
     </>
   );
 };
