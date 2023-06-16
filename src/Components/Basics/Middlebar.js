0a7ffa19e6afba3f0a541985a95e1e2df2fe46b2
ashmina <raiashmina404@gmail.com>
2023-06-16 13:56:48 +0545

    updated

--- src/Components/Basics/Middlebar.js
+++ src/Components/Basics/Middlebar.js
@@ -1,4 +1,9 @@
 import React from "react";
+// const breakPoints=[
+//   { width:1, itemsToShow: 1},
+//   { width:550, itemsToShow: 2},
+//   { width:768, itemsToShow: 3},
+//   { width:1200, itemsToShow: 4},
 
 const Middlebar = () => {
   return (
@@ -10,16 +15,58 @@ const Middlebar = () => {
             <h3>Stories</h3>
           </div>
           <div className="middlebar1">
-          <i class="fa-solid fa-film"></i>
+            <i class="fa-solid fa-film"></i>
             <h3>Reels</h3>
           </div>
           <div className="middlebar2">
-          <i class="fa-solid fa-video"></i>
+            <i class="fa-solid fa-video"></i>
             <h3>Rooms</h3>
           </div>
         </div>
         <hr className="middlehr" />
-        
+        {/* <div className="App">
+          <Carousel breakPoints={breakPoints}>
+            <Item>One</Item>
+            <Item>Two</Item>
+            <Item>Three</Item>
+            <Item>Four</Item>
+            <Item>Five</Item>
+            <Item>Six</Item>
+            <Item>Seven</Item>
+            <Item>Eight</Item>
+          </Carousel>
+        </div> */}
+      </div>
+      <div className="poststatus-container-content">
+        <div className="poststatus-container">
+          <div className="poststatus">
+            <img className="poststatusphoto" src="../Images/User.jpeg"></img>
+          </div>
+          <div className="poststatus1">
+            <input
+              type="text"
+              placeholder="What's on your mind, Ashmina?"
+              className="statusinput"
+            ></input>
+          </div>
+        </div>
+        <hr className="statushr" />
+        <div className="postvideo-container-content">
+          <div className="postvideo-container">
+            <div className="postvideo">
+              <i class="fa-solid fa-video livevideo"></i>
+              <h5 className="livevidos">Live Video</h5>
+            </div>
+            <div className="postvideo1">
+              <i class="fa-solid fa-video livevideo"></i>
+              <h5 className="livevidos">Live Video</h5>
+            </div>
+            <div className="postvideo2">
+              <i class="fa-solid fa-video livevideo"></i>
+              <h5 className="livevidos">Live Video</h5>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
