db29363b36b2da562f753aff48f766b738394d3f
ashmina <raiashmina404@gmail.com>
2023-06-17 23:48:23 +0545

    updated

--- src/Components/Basics/Middlebar.js
+++ src/Components/Basics/Middlebar.js
@@ -486,6 +486,38 @@ const Middlebar = () => {
        </div>
       </div>
 
+      <div className="newsfeed-container-content">
+        <div className="newsfeed-container">
+          <div className="newsfeed">
+            <img src="../Images/photo.jpeg" className="photoqu"></img>
+            {/* <h5 className="mq">Movie Quote</h5> */}
+          </div>
+          <div className="newsfeed1">
+            <span className="mq">Movie Quote</span>
+            {/* <br /> */}
+            <div>
+              <span className="date">1d.</span>
+              <i className="fas fa-earth"></i>
+            </div>
+          </div>
+
+          <div className="newsfeed3">
+            <i class="fa-solid fa-ellipsis"></i>
+          </div>
+          <div className="newsfeed4">
+          <i class="fa-solid fa-xmark"></i>
+
+          </div>
+        </div>
+
+       <div className="images-container-content">
+        <div className="images-container">
+          <img src="../Images/imagine.jpeg" className="care" alt=""></img>
+        </div>
+
+       </div>
+      </div>
+
       
 
       </section>
