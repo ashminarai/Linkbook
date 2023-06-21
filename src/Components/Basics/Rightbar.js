fbffc7763c2cb208388b45f45ca02ed252726a83
ashmina <raiashmina404@gmail.com>
2023-06-21 23:05:33 +0545

    rightbar updated

--- src/Components/Basics/Rightbar.js
+++ src/Components/Basics/Rightbar.js
@@ -4,35 +4,46 @@ import "./Style.css";
 const Rightbar = () => {
   return (
     <>
-      <h3 className="demosponsered">Sponsored</h3>
-      <img className="rightsponser" src="../Images/sponser.png" alt="sp"></img>
-      <hr className="contactshr" />
-      <div className="contacts-container-content">
-        <div className="contacts-container">
-          <h5>Contacts</h5>
+      <section className="rightsquad">
+        <h3 className="demosponsered">Sponsored</h3>
+        <img
+          className="rightsponser"
+          src="../Images/sponser.png"
+          alt="sp"
+        ></img>
+        <hr className="contactshr" />
+        <div className="contacts-container-content">
+          <div className="contacts-container">
+            <h3 className="rightcontactss">Contacts</h3>
+          </div>
+          <div className="contacts-container1">
+            <img
+              src="../Images/camcroder.png"
+              alt="cam"
+              className="camcroder"
+            ></img>
+          </div>
+          <div className="contacts-container2">
+            <img
+              src="../Images/investigate.png"
+              alt="invest"
+              className="investigate"
+            ></img>
+          </div>
+          <div className="contacts-container3">
+            <img
+              src="../Images/loader.png"
+              alt="invest"
+              className="investigate"
+            ></img>
+          </div>
         </div>
-        <div className="contacts-container1">
-          <img
-            src="../Images/camcroder.png"
-            alt="cam"
-            className="camcroder"
-          ></img>
-        </div>
-        <div className="contacts-container2">
-          <img
-            src="../Images/investigate.png"
-            alt="invest"
-            className="investigate"
-          ></img>
-        </div>
-        <div className="contacts-container3">
-          <img
-            src="../Images/loader.png"
-            alt="invest"
-            className="investigate"
-          ></img>
-        </div>
-      </div>
+        <img
+          src="../Images/contactli.png"
+          alt="contact"
+          className="contlist"
+        ></img>
+      </section>
     </>
   );
 };
