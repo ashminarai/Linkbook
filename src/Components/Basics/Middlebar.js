b631edc107b072ae024a985432243346f8eb70dd
ashmina <raiashmina404@gmail.com>
2023-06-23 23:32:59 +0545

    updated

--- src/Components/Basics/Middlebar.js
+++ src/Components/Basics/Middlebar.js
@@ -167,36 +167,7 @@ const Middlebar = () => {
           </div>
 
 
-          <div className="reels-container4">
-            <video controls className="tiktok">
-              <source src="../Videos/tiktok.mp4" className="tiktok" />
-            </video>
-          </div>
-          <div className="reels-container4">
-            <video controls className="tiktok">
-              <source src="../Videos/tiktok.mp4" className="tiktok" />
-            </video>
-          </div>
-          <div className="reels-container4">
-            <video controls className="tiktok">
-              <source src="../Videos/tiktok.mp4" className="tiktok" />
-            </video>
-          </div>
-          <div className="reels-container4">
-            <video controls className="tiktok">
-              <source src="../Videos/tiktok.mp4" className="tiktok" />
-            </video>
-          </div>
-          <div className="reels-container4">
-            <video controls className="tiktok">
-              <source src="../Videos/tiktok.mp4" className="tiktok" />
-            </video>
-          </div>
-          <div className="reels-container4">
-            <video controls className="tiktok">
-              <source src="../Videos/tiktok.mp4" className="tiktok" />
-            </video>
-          </div>
+          
 
 
 
