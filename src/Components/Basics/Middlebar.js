e0e04876377a532cffbe24550b455c67c3e7f1f3
ashmina <raiashmina404@gmail.com>
2023-06-23 23:27:09 +0545

    updated

--- src/Components/Basics/Middlebar.js
+++ src/Components/Basics/Middlebar.js
@@ -167,7 +167,36 @@ const Middlebar = () => {
           </div>
 
 
-          
+
+
+
+
+
+
+
+
+
+
+
+          <div className="reels-container4">
+            <video controls className="tiktok">
+              <source src="../Videos/tiktok.mp4" className="tiktok" />
+            </video>
+          </div>
+
+
+          <div className="reels-container4">
+            <video controls className="tiktok">
+              <source src="../Videos/tiktok.mp4" className="tiktok" />
+            </video>
+          </div>
+
+
+          <div className="reels-container4">
+            <video controls className="tiktok">
+              <source src="../Videos/tiktok.mp4" className="tiktok" />
+            </video>
+          </div>
 
           
 
