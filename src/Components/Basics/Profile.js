35430eccc8bb2e1f09d18a077250fb78eb28ea0c
ashmina <raiashmina404@gmail.com>
2023-07-06 22:14:12 +0545

    updated

--- src/Components/Basics/Profile.js
+++ src/Components/Basics/Profile.js
@@ -1,11 +1,14 @@
 import React from 'react'
+// import { Link } from "react-router-dom";
+
 
 
 const Profile = () => {
   return (
    <>
+   This is a demo
    <div>
-    this is a demo.
+  
    </div>
    </>
   )
